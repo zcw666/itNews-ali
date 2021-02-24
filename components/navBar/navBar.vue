e7f111d0f518c0c6fc42fb2c8c92fa2dc3ace701
chengwen <webgit@163.com>
2021-02-25 00:16:32 +0800

    navBar适配

--- components/navBar/navBar.vue
+++ components/navBar/navBar.vue
@@ -1,15 +1,21 @@
 <template>
 	<view class="com-navbar">
 		<view class="navbar-fixed">
-			<view class="navbar-search">
-				<view class="iconfont icon-search">
-					
-				</view>
-				<view class="navbar-search-btn"></view>
+			<view class="" :style="{height: statusBarHeight + 'px'}">
+				
 			</view>
+			<view class="content" :style="{height:paddingHeight*2+navBarHeight+'px',width:windowWidth+'px'}" >
+				<view class="navbar-search">
+					<view class="iconfont icon-search">
+						
+					</view>
+					<view class="navbar-search-btn"></view>
+				</view>
+			</view>
+			
 		</view>
 		
-		<view class="nav-hold" style="height: 45px;">
+		<view class="nav-hold" :style="{height: paddingHeight*2+navBarHeight+statusBarHeight +'px;'}">
 			
 		</view>
 	</view>
@@ -19,8 +25,25 @@
 	export default {
 		data() {
 			return {
-				
+				statusBarHeight:20,
+				navBarHeight:45,
+				paddingHeight:4,
+				windowWidth:375
 			};
+		},
+		created() {
+			const res = uni.getSystemInfoSync();
+			this.statusBarHeight = res.statusBarHeight
+			console.log(res)
+			 // #ifndef  H5 || APP-PLUS || MP-ALIPAY
+			 
+			
+			let menuButtonInfo = uni.getMenuButtonBoundingClientRect()
+			console.log(menuButtonInfo)
+			this.paddingHeight = menuButtonInfo.top-res.statusBarHeight
+			this.navBarHeight = menuButtonInfo.height
+			this.windowWidth = menuButtonInfo.left
+			// #endif
 		}
 	}
 </script>
@@ -33,27 +56,32 @@
 			top: 0;
 			left: 0;
 			z-index: 99;
-			display: flex;
-			justify-content: center;
-			align-items: center;
 			width: 100%;
-			height: 45px;
 			background-color: $mk-base-color;
-			.navbar-search{
-				box-sizing: border-box;
-				width: 80%;
+			.content{
 				display: flex;
+				justify-content: center;
 				align-items: center;
-				height: 40px;
-				border-radius: 20px;
-				background-color: #fff;
+				box-sizing: border-box;
+				padding: 0 10px;
+				// width: 100px;
+				.navbar-search{
+					width: 100%;
+					display: flex;
+					align-items: center;
+					height: 40px;
+					padding: 0 10px;
+					border-radius: 20px;
+					background-color: #fff;
+				}
+				.navbar-search-icon{
+					width: 10px;
+					height: 10px;
+					background-color: $mk-base-color;
+					border-radius: 20px;
+				}
 			}
-			.navbar-search-icon{
-				width: 10px;
-				height: 10px;
-				background-color: $mk-base-color;
-				border-radius: 20px;
-			}
+			
 		}
 		
 	}
